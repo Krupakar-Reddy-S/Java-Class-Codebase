745b597d6a63b6043438a92854231c81f45ea933
Krupakar Reddy <krupakarreddysuda0@gmail.com>
2023-08-30 19:12:06 +0530

    Cleaned up Image Editor with custom File Output

--- JavaFiles-CP/ImageEditor/ImageEditor.java
+++ JavaFiles-CP/ImageEditor/ImageEditor.java
@@ -1,20 +1,16 @@
 import java.io.File;
-import java.lang.*;
 import java.util.*;
 import java.awt.*;
-
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.awt.image.BufferedImage;
 
 public class ImageEditor {
 
-    // FUNCTION TO INCREASE BRIGHTNESS OF IMAGE
-
     public static BufferedImage increaseBrightness(BufferedImage input, int a) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage OutputImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 Color pixel = new Color(input.getRGB(j, i));
@@ -37,94 +33,94 @@ public class ImageEditor {
                 if (blue < 0)
                     blue = 0;
                 Color newpixel = new Color(red, green, blue);
-                output.setRGB(j, i, newpixel.getRGB());
+                OutputImage.setRGB(j, i, newpixel.getRGB());
             }
         }
-        return output;
+        return OutputImage;
     }
 
     public static BufferedImage converttogray(BufferedImage input) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        BufferedImage OutputImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                output.setRGB(j, i, input.getRGB(j, i));
+                OutputImage.setRGB(j, i, input.getRGB(j, i));
             }
         }
-        return output;
+        return OutputImage;
     }
 
     public static BufferedImage rightRotate(BufferedImage input) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(height, width, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage OutputImage = new BufferedImage(height, width, BufferedImage.TYPE_3BYTE_BGR);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                output.setRGB(i, j, input.getRGB(j, i));
+                OutputImage.setRGB(i, j, input.getRGB(j, i));
             }
         }
-        int cols = output.getWidth() - 1;
-        for (int i = 0; i < output.getHeight(); i++) {
-            for (int j = 0; j < (output.getWidth() / 2); j++) {
-                Color pixel = new Color(output.getRGB(j, i));
-                output.setRGB(j, i, output.getRGB(cols - j, i));
-                output.setRGB(cols - j, i, pixel.getRGB());
+        int cols = OutputImage.getWidth() - 1;
+        for (int i = 0; i < OutputImage.getHeight(); i++) {
+            for (int j = 0; j < (OutputImage.getWidth() / 2); j++) {
+                Color pixel = new Color(OutputImage.getRGB(j, i));
+                OutputImage.setRGB(j, i, OutputImage.getRGB(cols - j, i));
+                OutputImage.setRGB(cols - j, i, pixel.getRGB());
             }
         }
-        return output;
+        return OutputImage;
     }
 
     public static BufferedImage leftRotate(BufferedImage input) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(height, width, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage OutputImage = new BufferedImage(height, width, BufferedImage.TYPE_3BYTE_BGR);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                output.setRGB(i, j, input.getRGB(j, i));
+                OutputImage.setRGB(i, j, input.getRGB(j, i));
             }
         }
-        int rows = output.getHeight() - 1;
-        for (int j = 0; j < output.getWidth(); j++) {
-            for (int i = 0; i < output.getHeight() / 2; i++) {
-                Color pixel = new Color(output.getRGB(j, i));
-                output.setRGB(j, i, output.getRGB(j, rows - i));
-                output.setRGB(j, rows - i, pixel.getRGB());
+        int rows = OutputImage.getHeight() - 1;
+        for (int j = 0; j < OutputImage.getWidth(); j++) {
+            for (int i = 0; i < OutputImage.getHeight() / 2; i++) {
+                Color pixel = new Color(OutputImage.getRGB(j, i));
+                OutputImage.setRGB(j, i, OutputImage.getRGB(j, rows - i));
+                OutputImage.setRGB(j, rows - i, pixel.getRGB());
             }
         }
-        return output;
+        return OutputImage;
     }
 
     public static BufferedImage verticallyInvert(BufferedImage input) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(width, height,BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage OutputImage = new BufferedImage(width, height,BufferedImage.TYPE_3BYTE_BGR);
         for (int j = 0; j < width; j++) {
             for (int i = 0; i < height / 2; i++) {
                 Color temp = new Color(input.getRGB(j, i));
-                input.setRGB(j, i, input.getRGB(j, height - 1 - i));
-                input.setRGB(j, height - 1 - i, temp.getRGB());
+                OutputImage.setRGB(j, i, input.getRGB(j, height - 1 - i));
+                OutputImage.setRGB(j, height - 1 - i, temp.getRGB());
             }
         }
-        return input;
+        return OutputImage;
     }
 
     public static BufferedImage horizantallyInvert(BufferedImage input) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage OutputImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                output.setRGB(width - 1 - j, i, input.getRGB(j, i));
+                OutputImage.setRGB(width - 1 - j, i, input.getRGB(j, i));
             }
         }
-        return output;
+        return OutputImage;
     }
 
     public static BufferedImage blurr(BufferedImage input, int k) {
         int height = input.getHeight();
         int width = input.getWidth();
-        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage OutputImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 
         int startrow = 0;
         while (startrow < height) {
@@ -148,7 +144,7 @@ public class ImageEditor {
                 for (int i = startrow; i < startrow + k; i++) {
                     for (int j = startcols; j < startcols + k; j++) {
                         Color newpixel = new Color(finalred, finalgreen, finalblue);
-                        output.setRGB(j, i, newpixel.getRGB());
+                        OutputImage.setRGB(j, i, newpixel.getRGB());
 
                     }
                 }
@@ -156,71 +152,85 @@ public class ImageEditor {
             }
             startrow += k;
         }
-        return output;
+        return OutputImage;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the path of image file");
-        String s = sc.next();
+        System.out.println();
+        System.out.print("Enter the path of image file: ");
 
-        File inputFile = new File(s);
+        String ImagePath = sc.next();
+        String FileExtension = "" + ImagePath.charAt(ImagePath.length()-3) + ImagePath.charAt(ImagePath.length()-2) + ImagePath.charAt(ImagePath.length()-1);
+
+        File inputFile = new File(ImagePath);
         try {
             BufferedImage inputImage = ImageIO.read(inputFile);
-            System.out.println(
-                    "enter 1 to print pixels \nenter 2 to get grayimage \nenter 3 to increase brightness \nenter 4 to rotate the image\n enter 5 to vertically invert the Image \n enter 6 to horizantally invert the image ");
-            int todo = sc.nextInt();
-            if (todo == 1){
-                // printpixels(inputImage);
-            }
-            else if (todo == 2) {
-                BufferedImage outputImage = converttogray(inputImage);
-                File output = new File("output.jpg");
-                ImageIO.write(outputImage, "jpg", output);
-            } else if (todo == 3) {
-                System.out.println("By how much percent you want to change the brightness");
-                int a = sc.nextInt();
-                BufferedImage brighterimage = increaseBrightness(inputImage, a);
-                File output = new File("output.jpg");
-                ImageIO.write(brighterimage, "jpg", output);
-            }
+            File OutputImage = new File("OutputImage." + FileExtension);
+            
+            System.out.println("Enter the Operation to perform: ");
+            System.out.println("1. Convert Image to Grayscale");
+            System.out.println("2. Increase Brightness of Image");
+            System.out.println("3. Rotate Image");
+            System.out.println("4. Invert Image Vertically");
+            System.out.println("5. Invert Image Horizontally");
+            System.out.println();
 
-            else if (todo == 4) {
-                System.out.println("enter 1 to rotate towards right and enter 2 to rotate towards left");
-                int x = sc.nextInt();
-                if (x == 1) {
-                    BufferedImage outputImage = rightRotate(inputImage);
-                    File output = new File("output.jpg");
-                    ImageIO.write(outputImage, "jpg", output);
+            int Operation = sc.nextInt();
+            switch (Operation){
 
-                } else if (x == 2) {
-                    BufferedImage outputImage = leftRotate(inputImage);
-                    File output = new File("output.jpg");
-                    ImageIO.write(outputImage, "jpg", output);
-                }
-            } else if (todo == 5) {
-                BufferedImage outputImage = verticallyInvert(inputImage);
-                File output = new File("output.jpg");
-                ImageIO.write(outputImage, "jpg", output);
-            } else if (todo == 6) {
-                BufferedImage outputImage = horizantallyInvert(inputImage);
-                File output = new File("output.jpg");
-                ImageIO.write(outputImage, "jpg", output);
-            } else if (todo == 7) {
-                int height = inputImage.getHeight();
-                int width = inputImage.getWidth();
-                System.out.println("on the scale of 1 to" + Math.min(height, width)
-                        + " , select the level at which you want to bluur the image");
-                int k = sc.nextInt();
-                BufferedImage outputImage = blurr(inputImage, k);
-                File output = new File("output.jpg");
-                ImageIO.write(outputImage, "jpg", output);
+                case 1:
+                    BufferedImage GreyScaleImage = converttogray(inputImage);
+                    ImageIO.write(GreyScaleImage, FileExtension, OutputImage);
+                    break;
+
+                case 2:
+                    System.out.println("By how much percent you want to change the brightness");
+                    int a = sc.nextInt();
+                    BufferedImage BrighterImage = increaseBrightness(inputImage, a);
+                    ImageIO.write(BrighterImage, FileExtension, OutputImage);
+                    break;
+
+                case 3:
+                    System.out.println("enter 1 to rotate towards right and enter 2 to rotate towards left");
+                    int x = sc.nextInt();
+                    if (x == 1) {
+                        BufferedImage RotatedImage = rightRotate(inputImage);
+                        ImageIO.write(RotatedImage, FileExtension, OutputImage);
+                    } 
+                    else if (x == 2) {
+                        BufferedImage RotatedImage = leftRotate(inputImage);
+                        ImageIO.write(RotatedImage, FileExtension, OutputImage);
+                    }
+                    break;
+                
+                case 4:
+                    BufferedImage VerticalImage = verticallyInvert(inputImage);
+                    ImageIO.write(VerticalImage, FileExtension, OutputImage);
+                    break;
+
+                case 5:
+                    BufferedImage HorizontalImage = horizantallyInvert(inputImage);
+                    ImageIO.write(HorizontalImage, FileExtension, OutputImage);
+                    break;
+                
+                case 6:
+                    int height = inputImage.getHeight();
+                    int width = inputImage.getWidth();
+                    System.out.println("on the scale of 1 to " + Math.min(height, width)+ " , select the level at which you want to bluur the image");
+                    int k = sc.nextInt();
+                    BufferedImage BlurredImage = blurr(inputImage, k);
+                    ImageIO.write(BlurredImage, FileExtension, OutputImage);
+                    break;
+
             }
-        } catch (Exception e) {
-            System.out.println("Error!");
-            // TODO: handle exception
+        } 
+
+        catch (IOException e) {
+            System.out.println("Please Enter Valid Image Path!");
         }
 
+        sc.close();
     }
 }
