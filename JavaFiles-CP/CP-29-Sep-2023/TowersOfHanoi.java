ed5aca9b0efb4ad477235d280bb1d5fb6141e70c
Krupakar Reddy <krupakarreddysuda0@gmail.com>
2023-09-30 22:00:01 +0530

    Daily Code Update

---
+++ JavaFiles-CP/CP-29-Sep-2023/TowersOfHanoi.java
@@ -0,0 +1,18 @@
+public class TowersOfHanoi {
+
+    /* 
+     * There are three towers A, B and C.
+     * N no. of disks are placed in tower A sorted based on diameter.
+     * 
+     * You must move the disks from A to C, with picking one disk at a time,
+     * and you cannot place a disk of smaller diameter below a larger disk.
+     * 
+     * Tower B can be used to carry out the required movements.
+     * Return the minimum no. of steps to move the disk arrangement.
+     * 
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
